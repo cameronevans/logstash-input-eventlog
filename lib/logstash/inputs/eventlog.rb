0f9a787c098a46df3728427cf686bf5c40880b5c
cameronevans <cameron@toplog.io>
2015-05-28 16:29:29 -0300

    )

--- lib/logstash/inputs/eventlog.rb
+++ lib/logstash/inputs/eventlog.rb
@@ -99,7 +99,7 @@ class LogStash::Inputs::EventLog < LogStash::Inputs::Base
       decorate(e)
 
       e["insertion"] = parse_insertion(e["message"])
-      e["message"] = event.Message.split(Regexp.New('(\\r|\\n|\\t)+').first
+      e["message"] = event.Message.split(Regexp.New('(\\r|\\n|\\t)+')).first
 
       queue << e
 
